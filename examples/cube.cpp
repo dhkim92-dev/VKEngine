4b454b485b843b33d4bab43df41f9f3374311631
dohoon <elensar92@github.com>
2021-08-28 13:47:37 +0900

    volume-rendering-marchingcube

--- examples/cube.cpp
+++ examples/cube.cpp
@@ -22,32 +22,6 @@
 using namespace std;
 using namespace VKEngine;
 
-
-struct {
-	string file_path;
-	struct _shape{
-		uint32_t x;
-		uint32_t y;
-		uint32_t z;
-	}shape;
-	float target_isovalue;
-}VolumeMetaInfo;
-
-float* loadVolume(){
-	char *data;
-	std::ifstream is(VolumeMetaInfo.file_path, std::ios::binary | std::ios::in | std::ios::ate);
-	if(is.is_open()){
-		size_t size = is.tellg();
-		assert(size > 0);
-		is.seekg(0, std::ios::beg);
-		char *data = new char[size];
-		is.read(data, size);
-	}else{
-		cerr << "fail to read volume data. check it first\n";
-	}	
-	return (float *)data;
-};
-
 vector<const char *> getRequiredExtensions(  ){
 	glfwInit();
 	uint32_t glfwExtensionCount = 0;
@@ -92,6 +66,10 @@ struct Vertex {
 	}
 };
 
+struct Cube{
+	Buffer *vbo;
+	Buffer *ibo;
+}cube;
 
 struct RenderObject{
 	struct {
@@ -104,37 +82,41 @@ struct RenderObject{
 	Program *program = nullptr;
 };
 
+vector<Vertex> cube_vertices = {
+	
+	{{-0.5f, 0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}}, 
+	{{0.5f, 0.5f, -0.5f}, {0.5f, 0.5f, 0.0f}},
+	{{0.5f, 0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+	{{-0.5f, 0.5f, 0.5f}, {0.0f, 0.5f, 0.5f}},
+	{{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}},
+	{{0.5f, -0.5f, -0.5f}, {0.0f, 0.5f, 0.5f}},
+	{{0.5f, -0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+	{{-0.5f, -0.5f, 0.5f}, {0.5f, 0.5f, 0.0f}}
+};
+
+vector<uint16_t> cube_indices = {
+	
+	0,1,5,
+	5,4,0,
+	1,2,5,
+	5,2,6,
+	6,7,5,
+	5,7,4,
+	1,0,3,
+	3,2,1,
+	2,3,7,
+	7,6,2,
+	7,3,0,
+	0,4,7
+};
+
 class App : public VKEngine::Application{
 	public :
-
 	explicit App(string app_name, string engine_name, int h, int w, vector<const char*>instance_exts, vector<const char*>device_exts , vector<const char *>valids) : Application(app_name, engine_name, h, w, instance_exts, device_exts, valids){
 	};
 
 	vector<RenderObject> render_objects;
 	unordered_map<string, Program*> programs;
-	Kernel volume_test, cube_test, edge_test;
-
-	struct {
-		struct{
-			Buffer raw;
-		}host;
-		struct{
-			Buffer volume_test;
-			Buffer edge_test;
-			Buffer scan_result;
-			Buffer ibo;
-			Buffer vbo;
-		}device;
-		void destroy(){
-			host.raw.destroy();
-			volume_test.destroy();
-			edge_test.destroy();
-			scan_result.destroy();
-			ibo.destroy();
-			vbo.destroy();
-		};
-	}volume;
-
 	protected:
 	
 	virtual void initWindow(){
@@ -153,7 +135,9 @@ class App : public VKEngine::Application{
 		while(!glfwWindowShouldClose(window)){
 			glfwPollEvents();
 			draw();
+			updateUniforms();
 		}
+		cleanup();
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
@@ -162,14 +146,6 @@ class App : public VKEngine::Application{
 		render();
 	}
 
-	void prepareVolume(){
-		volume.raw.create(context, VK_BUFFER_USAGE_)
-	}
-
-	void prepareCompute(){
-		Kernel 
-	}
-	
 	void preparePrograms(){
 		LOG("-------------Test::preparePrograms() start------------------------\n");
 		Program *program = new Program(context);
@@ -190,6 +166,35 @@ class App : public VKEngine::Application{
 	}
 
 	void prepareRenderObjects(){
+		LOG("-----------------------Test::prepareRenderObjects() start-----------------------\n");
+		uint32_t nr_cubes = 1;
+		uint32_t sz_mvp = sizeof(render_objects[0].matices);	
+		size_t sz_vertex = sizeof(Vertex) * cube_vertices.size();
+		size_t sz_index = sizeof(uint16_t) * cube_indices.size();
+		render_objects.resize(nr_cubes);
+		LOG("sz_mvp : %d\nsz_vertex : %d\nsz_index: % d\n", sz_mvp, sz_vertex, sz_index);
+
+		LOG("descriptorSet : %p \n", render_objects[0].descriptor_set);
+		for(uint32_t i = 0 ; i < nr_cubes ; ++i){
+			render_objects[i].program = programs["cube"];
+			render_objects[i].matices.model = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0, 0.0f));
+			render_objects[i].matices.view = camera.matrices.view;
+			render_objects[i].matices.proj = camera.matrices.proj;
+			render_objects[i].ubo = new Buffer(context, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, 
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, sz_mvp, &render_objects[i].matices);
+			render_objects[i].program->allocDescriptorSet(&render_objects[i].descriptor_set, 0);
+		}
+		LOG("descriptorSet : %p \n", render_objects[0].descriptor_set);
+		writeDescriptors();
+		LOG("index buffer object : %p \n", cube.ibo);
+		cube.vbo = new Buffer( context, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT| VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, sz_vertex, nullptr);
+		cube.ibo = new Buffer(context, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, sz_index , nullptr);
+		graphics_queue->enqueueCopy(cube_vertices.data(), cube.vbo, 0, 0, sz_vertex);
+		graphics_queue->enqueueCopy(cube_indices.data(), cube.ibo, 0, 0, sz_index);
+
+		LOG("index buffer object : %p \n", cube.ibo);
+		LOG("Index Buffer Object address : %p \n", VkBuffer(*cube.ibo));
+		LOG("-----------------------Test::prepareRenderObjects() end-----------------------\n");
 	}
 
 	void prepareCommandBuffer(){
@@ -209,9 +214,8 @@ class App : public VKEngine::Application{
 		for(uint32_t i = 0 ; i < draw_command_buffers.size() ; ++i){
 			draw_command_buffers[i] = graphics_queue->createCommandBuffer(VK_COMMAND_BUFFER_LEVEL_PRIMARY);
 		}
-		
+
 		for(uint32_t i = 0 ; i < draw_command_buffers.size() ; ++i){
-			/*
 			graphics_queue->beginCommandBuffer(draw_command_buffers[i]);
 			render_pass_BI.framebuffer = framebuffers[i];
 			VkViewport viewport = infos::viewport(static_cast<float>(width), static_cast<float>(height), 0.0f, 1.0f);
@@ -234,10 +238,56 @@ class App : public VKEngine::Application{
 			vkCmdDrawIndexed(draw_command_buffers[i], static_cast<uint32_t>(cube_indices.size()), 1, 0, 0, 0);
 			vkCmdEndRenderPass(draw_command_buffers[i]);
 			graphics_queue->endCommandBuffer(draw_command_buffers[i]);
-			*/
 		}	
 	}
 
+	void cleanupRenderObjects(){
+		VkDevice device = VkDevice(*context);
+		if(render_objects[0].descriptor_set != VK_NULL_HANDLE)
+			render_objects[0].program->releaseDescriptorSet(&render_objects[0].descriptor_set);
+		render_objects[0].program = nullptr;
+
+		delete cube.vbo;
+		delete cube.ibo;
+		LOG("cleanup Render objects\n");
+	}
+
+	void cleanupPrograms(){
+		LOG("cleanup Programs\n");
+		for(auto &program : programs){
+			program.second->destroy();
+		}
+	}
+
+	void cleanup(){
+		cleanupRenderObjects();
+		cleanupPrograms();
+	}
+
+	void writeDescriptors(){
+		LOG("Test::writeDescriptors() start \n");
+		for(auto &render_object : render_objects){
+			render_object.program->uniformUpdate(
+				render_object.descriptor_set,
+				VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0,
+				&render_object.ubo->descriptor, nullptr
+			);
+		}
+		LOG("Test::writeDescriptors() end \n");
+	}
+
+	void updateUniforms(){
+		static auto startTime = std::chrono::high_resolution_clock::now();
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+		for(auto &obj : render_objects){
+			obj.matices.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));//glm::rotate(obj.matices.model, glm::radians(15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+			obj.matices.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));//camera.matrices.view;
+			obj.matices.proj = glm::perspective(glm::radians(45.0f), width / (float)height, 0.1f, 10.0f);//camera.matrices.proj;
+			obj.matices.proj[1][1] *= -1;
+			obj.ubo->copyFrom(&obj.matices, sizeof(obj.matices));
+		}
+	}
 
 	public:
 	void run(){
@@ -249,6 +299,7 @@ class App : public VKEngine::Application{
 	}
 };
 
+
 int main(int argc, char *argv[])	
 {
 	vector<const char*> instance_extensions(getRequiredExtensions());
@@ -256,17 +307,6 @@ int main(int argc, char *argv[])
 	vector<const char *>device_extensions={VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 	string _name = "vulkan";
 	string engine_name = "engine";
-
-	string file_path(argv[1]);
-	int x = atoi(argv[2]);
-	int y = atoi(argv[3]);
-	int z = atoi(argv[4]);
-	float isovalue = atof(argv[5]);
-	VolumeMetaInfo.file_path = file_path;
-	VolumeMetaInfo.shape.x = x;
-	VolumeMetaInfo.shape.y = y;
-	VolumeMetaInfo.shape.z = z;
-	VolumeMetaInfo.target_isovalue = isovalue;
 
 	try {
 		App app(_name, engine_name, 600, 800, instance_extensions, device_extensions , validations);
---
+++ examples/matmul.cpp
@@ -0,0 +1,259 @@
+#include <iostream>
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <string>
+#include <array>
+#include <unordered_map>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <chrono>
+
+#ifndef GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN 1
+#endif
+
+#include "vk_core.h"
+#include "vk_application.h"
+#include "cstdio"
+
+using namespace std;
+using namespace VKEngine;
+
+vector<const char *> getRequiredExtensions(  ){
+	glfwInit();
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if(validationEnable) {
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+	glfwTerminate();
+	return extensions;
+}
+
+class MatmulTest{
+	struct KernelElement{
+		string name;
+		Kernel *kernel;
+	};
+	struct Shape{
+		uint32_t M = 1;
+		uint32_t N = 1;
+		uint32_t K = 1;
+	};
+
+	public : 
+	Context *context = nullptr;
+	VkDevice device = VK_NULL_HANDLE;
+	unordered_map<string, Kernel*> kernels;
+	CommandQueue *command_queue;
+	vector<Buffer *> d_buffers;
+	VkDescriptorPool descriptor_pool;
+	array<VkDescriptorSet, 3> descriptor_sets;
+	VkPipelineCache cache;
+
+	MatmulTest(Context *_context){
+		context = _context;
+		device = VkDevice(*context);
+		LOG("ComnmandQueue Created!\n");
+		command_queue = new CommandQueue(context, VK_QUEUE_COMPUTE_BIT);
+		LOG("ComnmandQueue Created!\n");
+	}
+
+	void init(){
+		VkPipelineCacheCreateInfo cache_CI = {};
+		cache_CI.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+		VK_CHECK_RESULT(vkCreatePipelineCache(device, &cache_CI, nullptr, &cache));
+	}
+
+	void createKernel(const string name, const string file_path){
+		LOG("createKernel::file_path : %s\n", file_path.c_str());
+		Kernel *kernel = new Kernel(context, file_path);
+		kernels.insert({name, kernel});
+	}
+
+	void setupDescriptorPool(){
+		std::array <VkDescriptorPoolSize, 1> pool_sizes={
+			{VK_DESCRIPTOR_TYPE_STORAGE_BUFFER , 12}
+		};
+		VkDescriptorPoolCreateInfo info = infos::descriptorPoolCreateInfo(1, pool_sizes.data(), 4);
+		VK_CHECK_RESULT(vkCreateDescriptorPool(VkDevice(*context), &info, nullptr, &descriptor_pool));
+		LOG("setupDescriptorPool done\n");
+	}
+
+	void setKernelLayout(){
+		kernels["matmul"]->setupDescriptorSetLayout({
+			infos::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 0),
+			infos::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 1),
+			infos::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 2),
+			infos::descriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 3)
+		});
+	}
+
+	void buildKernels(){
+		for(auto iter = kernels.begin() ; iter != kernels.end() ; ++iter){
+			iter->second->build(cache);
+		}
+	}
+
+	void setupDescriptorSets(){
+		kernels["matmul"]->allocateDescriptorSet(descriptor_pool);
+	}
+
+	void matmul(float *m1, float* m2, uint32_t M, uint32_t N, uint32_t K){
+		LOG("matmul called\n");
+		float *output = new float[M*N];
+
+		for(uint32_t i = 0 ; i < M*N ; ++i){
+			output[i] = i * 1.0f;
+		} 
+		Buffer d_m1(context, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			sizeof(float) * M * K,
+			nullptr
+		);
+		
+		Buffer d_m2(context, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			sizeof(float) * N * K,
+			nullptr
+		);
+
+		Buffer d_output(context, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			sizeof(float) * M * N,
+			nullptr
+		);
+
+		Buffer d_shape(context, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+			sizeof(uint32_t)*3,
+			nullptr
+		);
+		Shape shape = {M, N, K};
+		command_queue->enqueueCopy(m1, &d_m1, 0, 0, sizeof(float)*M*K);
+		command_queue->enqueueCopy(m2, &d_m2, 0, 0, sizeof(float)*N*K);
+		command_queue->enqueueCopy(&shape, &d_shape, 0, 0, sizeof(uint32_t)*3);
+
+		kernels["matmul"]->setKernelArgs(
+			{
+				{0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, &d_m1.descriptor, nullptr},
+				{1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, &d_m2.descriptor, nullptr},
+				{2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, &d_output.descriptor, nullptr},
+				{3, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, &d_shape.descriptor, nullptr}
+			}
+		);
+		command_queue->ndRangeKernel(kernels["matmul"], {M, N, 1}, VK_TRUE);
+		command_queue->enqueueCopy(&d_output, output, 0, 0, sizeof(float) * M * N);
+		for(uint32_t i = 0 ; i < M ; ++i){
+			for(uint32_t j = 0 ; j < N ; ++j){
+				printf("%.3f ", output[i*N + j]);
+			}
+			printf("\n");
+		}
+
+		d_m1.destroy();
+		d_m2.destroy();
+		d_shape.destroy();
+		d_output.destroy();
+		delete [] output;
+	}
+
+	void batch(){
+		
+		float mat1[9] = {
+			1.0f, 2.0f ,3.0f,
+			2.0f, 3.0f, 4.0f,
+			4.0f, 5.0f, 6.0f
+		};
+		float mat2[9] = {
+			0.0f, 0.0f, 1.0f,
+			0.0f, 1.0f, 0.0f,
+			1.0f, 0.0f, 0.0f
+		};
+
+		matmul(mat1, mat2, 3, 3, 3);
+				
+		float mat3[6] = {
+			// r x c = 3 x 2
+			1.0f, 1.0f,
+			2.0f, 2.0f,
+			3.0f, 3.0f
+			//in GPU
+		};
+
+		float mat4[8] = {
+			// r x c = 2 x 4
+			0.0f, 0.0f, 0.0f, 0.0f, 
+			1.0f, 1.0f, 1.0f, 1.0f 
+		};
+
+		// M N K = 3, 4, 2
+
+		/*
+		answer
+		r x c = 3 x 4
+		1.0f, 1.0f, 1.0f, 1.0f,
+		2.0f, 2.0f, 2.0f, 2.0f,
+		3.0,f 3.0f, 3.0f, 3.0f 
+		*/
+		
+		matmul(mat3, mat4, 3,4, 2);
+	}
+	
+	void run(){
+		init();
+		createKernel("matmul", "./shaders/matmul/matmul.comp.spv");
+		setupDescriptorPool();
+		setKernelLayout();
+		buildKernels();
+		setupDescriptorSets();
+		batch();
+	}
+
+	~MatmulTest(){
+		for(auto &iter : kernels){
+			//vkFreeDescriptorSets(device, descriptor_pool, 1, &iter.second->descriptors.set);
+			//iter.second->descriptors.set = VK_NULL_HANDLE;
+			iter.second->destroy();
+		}
+		vkDestroyDescriptorPool(device, descriptor_pool, nullptr);
+		vkDestroyPipelineCache(device, cache, nullptr);
+		delete command_queue;
+		delete context;
+	}
+};
+
+
+int main(int argc, char *argv[])	
+{
+	vector<const char*> instance_extensions(getRequiredExtensions());
+	vector<const char *> validations={"VK_LAYER_KHRONOS_validation"};
+	vector<const char *>device_extensions={VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+	string _name = "vulkan";
+	string engine_name = "engine";
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+	VkSurfaceKHR surface;
+
+	try {
+		Engine engine(_name, engine_name, instance_extensions, validations);
+		engine.init();
+		glfwCreateWindowSurface(VkInstance(engine), window, nullptr, &surface);
+		Context context(VkInstance(engine), 0, surface, VK_QUEUE_COMPUTE_BIT, device_extensions, validations);
+		MatmulTest test(&context);
+		test.run();
+	}catch(std::runtime_error& e){
+		cout << "error occured : " << e.what()  <<  "on File " << __FILE__ << " line : " << __LINE__ << "\n";
+		exit(EXIT_FAILURE);
+	};
+
+	return 0;
+}
